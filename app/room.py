a3c3a53621df8f6979e8baed493d11c595af29cf
Caroline Wanjiku <Caroline Wanjiku>
2017-11-20 14:16:30 +0300

    ft(reallocation)Fix failing tests on reallocation function

--- app/room.py
+++ app/room.py
@@ -5,25 +5,26 @@ class Room(object):
     """creates room object from where living space and office inherit """
     __metaclass__ = ABCMeta
     max_capacity = 0
+    members = []
 
-    def __init__(self, room_names, room_type):
-        self.room_name = room_names
+    def __init__(self, room_name, room_type):
+        self.room_name = room_name
         self.room_type = room_type
 
 
 class LivingSpace(Room):
     """inherits from Room class"""
-    living_space_members = []
+    # living_space_members = []
     max_capacity = 4
 
-    def __init__(self, room_names):
-        Room.__init__(self, room_names, "living space")
+    def __init__(self, room_name):
+        Room.__init__(self, room_name, "livingspace")
 
 
 class Office(Room):
     """inherits from Room class"""
-    office_members = []
+    # office_members = []
     max_capacity = 6
 
-    def __init__(self, room_names):
-        Room.__init__(self, room_names, "office")
+    def __init__(self, room_name):
+        Room.__init__(self, room_name, "office")
